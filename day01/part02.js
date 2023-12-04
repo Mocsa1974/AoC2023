04a073616821707b42b4e346ad1ec406bbed2dd7
Mocsa <mocsa1974@gmail.com>
2023-12-04 09:07:26 +0100

    20231204

--- day01/part02.js
+++ day01/part02.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const letters = {
-  "1": 1,
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  "one": 1,
-  "two": 2,
-  "three": 3,
-  "four": 4,
-  "five": 5,
-  "six": 6,
-  "seven": 7,
-  "eight": 8,
-  "nine": 9
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
 };
 
 console.log(
@@ -25,15 +25,22 @@ console.log(
     .readFileSync("./day01/input.txt", "utf-8")
     .split("\n")
     .map((line) => {
-      const f_numbers = line.trim().match(
-        /1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine/g
-      );
-      const l_numbers = line.trim().split('').reverse().join('').match(
-        /1|2|3|4|5|6|7|8|9|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/g
-      );
+      const f_numbers = line
+        .trim()
+        .match(
+          /1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine/g
+        );
+      const l_numbers = line
+        .trim()
+        .split("")
+        .reverse()
+        .join("")
+        .match(
+          /1|2|3|4|5|6|7|8|9|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/g
+        );
       const first = letters[f_numbers[0]];
-      const last = letters[l_numbers[0].split('').reverse().join('')];
-      return first.toString()+last.toString();
+      const last = letters[l_numbers[0].split("").reverse().join("")];
+      return first.toString() + last.toString();
     })
     .reduce((acc, curr) => {
       return acc + +curr;
--- day03/part02.js
+++ day03/part02.js
@@ -1,81 +1,87 @@
-const fs = require('fs');
+const fs = require("fs");
 const findNumbers = () => {
-    let result = [];
-    for (let i = 0; i < engine.length; i++) {
-        let n = '';
-        let start = -1;
-        let end = -1;
-        for (let j = 0; j < engine[i].length; j++) {
-            if (engine[i][j].number) {
-                n += engine[i][j].value;
-                if (start === -1) {
-                    start = j;
-                    end = -1;
-                }
-            } else {
-                if (!engine[i][j].number && start !==-1) {
-                    end = j-1;
-                    result.push({
-                        number:n,
-                        start:start,
-                        end:end,
-                        row:i
-                    });
-                    n= '';
-                    start = -1;
-                    end = -1;
-        
-                }
-            }
+  let result = [];
+  for (let i = 0; i < engine.length; i++) {
+    let n = "";
+    let start = -1;
+    let end = -1;
+    for (let j = 0; j < engine[i].length; j++) {
+      if (engine[i][j].number) {
+        n += engine[i][j].value;
+        if (start === -1) {
+          start = j;
+          end = -1;
         }
-        if (start !==-1 && end === -1) {
-            end =engine[0].length;
-            result.push({
-                number:n,
-                start:start,
-                end:end,
-                row:i
-            });
-
+      } else {
+        if (!engine[i][j].number && start !== -1) {
+          end = j - 1;
+          result.push({
+            number: n,
+            start: start,
+            end: end,
+            row: i,
+          });
+          n = "";
+          start = -1;
+          end = -1;
         }
+      }
     }
-    return result;
-}
+    if (start !== -1 && end === -1) {
+      end = engine[0].length;
+      result.push({
+        number: n,
+        start: start,
+        end: end,
+        row: i,
+      });
+    }
+  }
+  return result;
+};
 
-const engine = fs.readFileSync('./day03/input.txt', 'utf-8').split('\r\n')
-    .map((line,row) => {
-        return line.split('').map(char => {
-            return {
-                number: !isNaN(+char),
-                value: char,
-                symbol: isNaN(+char) && char !== '.',
-                period: char === '.',
-                row:row
-            }
-        })
-    })
+const engine = fs
+  .readFileSync("./day03/input.txt", "utf-8")
+  .split("\r\n")
+  .map((line, row) => {
+    return line.split("").map((char) => {
+      return {
+        number: !isNaN(+char),
+        value: char,
+        symbol: isNaN(+char) && char !== ".",
+        period: char === ".",
+        row: row,
+      };
+    });
+  });
 const gears = [];
 const numbers = findNumbers();
-engine.forEach((line,row) => {
-    for (let i=0;i<line.length;i++) {
-        if (line[i].symbol) gears.push({
-            row:row,
-            col:i
-        })
-    }
-    ;
+engine.forEach((line, row) => {
+  for (let i = 0; i < line.length; i++) {
+    if (line[i].symbol)
+      gears.push({
+        row: row,
+        col: i,
+      });
+  }
 });
 const result = [];
-gears.forEach(gear=>{
-    const filtered = numbers.filter(f=>{
-        return ((f.row===gear.row || f.row===gear.row -1 || f.row === gear.row + 1) 
-        && (gear.col>=f.start-1 && gear.col<=f.end+1));
-    });
-    if (filtered.length === 2) {
-        result.push(+filtered[0].number * +filtered[1].number);
-    }
-
+gears.forEach((gear) => {
+  const filtered = numbers.filter((f) => {
+    return (
+      (f.row === gear.row ||
+        f.row === gear.row - 1 ||
+        f.row === gear.row + 1) &&
+      gear.col >= f.start - 1 &&
+      gear.col <= f.end + 1
+    );
+  });
+  if (filtered.length === 2) {
+    result.push(+filtered[0].number * +filtered[1].number);
+  }
 });
-console.log(result.reduce((acc,curr) => {
-    return acc + +curr
-},0));
+console.log(
+  result.reduce((acc, curr) => {
+    return acc + +curr;
+  }, 0)
+);
